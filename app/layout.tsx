b5268dea252eca728e03b8713a833bb89f2b38fa
Ilyas Bouktrane <ilyas.bouktrane@gmail.com>
2025-11-01 22:56:48 -0400

    init

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "VLSM Calculator",
+  description:
+    "A tool that helps network engineers efficiently divide an IP network into subnets of varying sizes using Variable Length Subnet Masking (VLSM). It calculates subnet addresses, broadcast addresses, and host ranges for optimized IP allocation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
